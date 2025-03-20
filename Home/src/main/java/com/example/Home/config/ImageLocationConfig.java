c5ac0df7ef5ab001c1f886fcb97f7b4eb10b6162
chamalka Jayawardhana <chamalka8mini@gmail.com>
2025-03-20 10:38:23 +0530

    Fixed an error of image upload

---
+++ Home/src/main/java/com/example/Home/config/ImageLocationConfig.java
@@ -0,0 +1,15 @@
+package com.example.Home.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ImageLocationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:images/");
+    }
+}
